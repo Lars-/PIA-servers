4d40d98fb948b59d6e7ac8c252d0f9c54141d183
Lucas Magasweran <lucasrangit@gmail.com>
2022-03-04 11:01:49 +0100

    check if ip file exists

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,8 @@
 exports.handler = async (event, context) => {
+  client_ip = event.headers['x-nf-client-connection-ip'];
+  protected_status = File('/docs/' + client_ip).exists() ? "You are protected by PIA" : "You are not protected by PIA";
   return {
     statusCode: 200,
-    body: "Hello, " + event.headers['x-nf-client-connection-ip']
+    body: "Hello, " + client_ip + '\n' + protected_status,
   };
 };
